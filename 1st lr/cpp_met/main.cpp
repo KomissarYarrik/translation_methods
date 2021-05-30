e1380b7d56b4682ab8061b2de5381b9a5cd82a76
Алексей Фотин <alexsei.fotin@qfteam.net>
2021-05-30 03:08:17 +0300

    -

--- 1st lr/cpp_met/main.cpp
+++ 1st lr/cpp_met/main.cpp
@@ -1,3 +1,4 @@
+#pragma execution_character_set("utf-8")
 #include <iostream>
 #include <algorithm>
 #include <fstream> 
@@ -28,6 +29,8 @@ vector<string> readFileId()
     }     
     return listId;
 }
+
+
 
 void main()
 {
